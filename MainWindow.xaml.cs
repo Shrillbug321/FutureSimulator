cd3d5fb4c397d2cf79c48138e4876a3de08c1db4
Ja <Jakubiak-123@wp.pl>
2024-04-11 14:21:07 +0200

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FutureSimulator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+	private int rows = 6;
+	private int columns = 6;
+	private double cellWidth;
+	private double cellHeight;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		cellWidth = CellsCanvas.Width / columns;
+		cellHeight = CellsCanvas.Height / rows;
+		
+		for (int i = 0; i < columns; i++)
+		{
+			for (int j = 0; j < rows; j++)
+			{
+				Rectangle r = new()
+				{
+					Width = cellWidth,
+					Height = cellHeight,
+					Fill = new SolidColorBrush(Color.FromRgb((byte)(7*i), (byte)(8*j), (byte)(20+i))),
+					Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255))
+				};
+				Canvas.SetTop(r, i * cellHeight);
+				Canvas.SetLeft(r, j * cellWidth);
+				CellsCanvas.Children.Add(r);
+			}
+		}
+	}
+}
